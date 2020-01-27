aeb0e669fe2c7039d4a3550358bf8fcdda855502
Alex Lee <leeeaj@bc.edu>
2020-01-27 16:21:55 -0500

    Commit 2

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Alex Dongeun Lee on 1/26/20.
+//  Copyright © 2020 Alex Dongeun Lee. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imageNumber = -1
+    var messageNumber = -1
+    let totalNumberOfImages = 9
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "Fabulous? That's You!",
+                        "You've Got The Design Skills of John Ive"]
+        
+        var newMessageNumber: Int
+        repeat {
+            newMessageNumber = Int.random(in: 0...messages.count-1)
+        } while messageNumber == newMessageNumber
+        messageNumber = newMessageNumber
+        messageLabel.text = messages[messageNumber]
+        
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
+        
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: \(error.localizedDescription) Could not initialize AudioPlayer object")
+            }
+        } else {
+            print(" ERROR: Could not read data from file sound0")
+        }
+    }
+
+
+}
+
